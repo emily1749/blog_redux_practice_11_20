fd6dc8e5a18c24d196d2ae3440869046ca91b825
Emily <linemily97@gmail.com>
2020-11-20 22:03:13 -0800

    redux tunk

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostList extends Component {
+  render() {
+    return <div>Post List</div>;
+  }
+}
+
+export default connect(null, { fetchPosts: fetchPosts })(PostList);
