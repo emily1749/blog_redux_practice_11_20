d7baa4f1c89e4fe2168f863c5d956668af19d28f
Emily <linemily97@gmail.com>
2020-11-21 14:26:08 -0800

    edits postlist

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,17 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS',
-  };
+import Axios from 'axios';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+//same as !! below
+// export const fetchPosts = () => {
+//   return async function(dispatch, getState) {
+//     const response = await jsonPlaceholder.get('/posts');
+
+//     dispatch({ type: 'FETCH_POSTS', payload: response });
+//   };
+// };
